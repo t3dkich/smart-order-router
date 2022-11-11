bf48c73d4b3e4ffacdd7b5e192108e79bddaf6f3
Teodor Zafirov <36457245+t3dkich@users.noreply.github.com>
2022-11-11 17:37:43 +0200

    feat: maistestsubnet

---
+++ src/util/addresses.ts
@@ -0,0 +1,167 @@
+import { Token } from '@uniswap/sdk-core';
+import { FACTORY_ADDRESS } from '@uniswap/v3-sdk';
+import { ChainId, NETWORKS_WITH_SAME_UNISWAP_ADDRESSES } from './chains';
+
+const CELO_V3_CORE_FACTORY_ADDRESSES =
+  '0xAfE208a311B21f13EF87E33A90049fC17A7acDEc';
+const CELO_QUOTER_ADDRESSES = '0x82825d0554fA07f7FC52Ab63c961F330fdEFa8E8';
+const CELO_MULTICALL_ADDRESS = '0x633987602DE5C4F337e3DbF265303A1080324204';
+
+const MAISTESTSUBNET_FACTORY_V3 = '0x466B8A8975310B19EF65D93b23F8a287a3A6a5E4'
+const MAISTESTSUBNET_QUOTER_V2 = '0x41D79cf1711bc5b56b8dBbaddDaC7bdb0BBD71Ef'
+const MAISTESTSUBNET_MIXED_ROUTE_QUOTER_V1 = '0x92C5071404ed464925E28A8164eE846279Dda647'
+// const MAISTESTSUBNET_MULTICALL = '0x77485F0CD4162406914F9090e3CE57596FF46068'
+const MAISTESTSUBNET_MULTICALL = '0xF0827a7effa4b0C434F519cda72D11E90A31575d'
+
+export const V3_CORE_FACTORY_ADDRESSES: AddressMap = {
+  ...constructSameAddressMap(FACTORY_ADDRESS),
+  [ChainId.CELO]: CELO_V3_CORE_FACTORY_ADDRESSES,
+  [ChainId.CELO_ALFAJORES]: CELO_V3_CORE_FACTORY_ADDRESSES,
+  [ChainId.MAISTESTSUBNET]: MAISTESTSUBNET_FACTORY_V3
+
+  // TODO: Gnosis + Moonbeam contracts to be deployed
+};
+
+export const QUOTER_V2_ADDRESSES: AddressMap = {
+  ...constructSameAddressMap('0x61fFE014bA17989E743c5F6cB21bF9697530B21e'),
+  [ChainId.CELO]: CELO_QUOTER_ADDRESSES,
+  [ChainId.CELO_ALFAJORES]: CELO_QUOTER_ADDRESSES,
+  [ChainId.MAISTESTSUBNET]: MAISTESTSUBNET_QUOTER_V2
+
+  // TODO: Gnosis + Moonbeam contracts to be deployed
+};
+
+export const MIXED_ROUTE_QUOTER_V1_ADDRESSES: AddressMap = {
+  [ChainId.MAINNET]: '0x84E44095eeBfEC7793Cd7d5b57B7e401D7f1cA2E',
+  [ChainId.RINKEBY]: '0x84E44095eeBfEC7793Cd7d5b57B7e401D7f1cA2E',
+  [ChainId.ROPSTEN]: '0x84E44095eeBfEC7793Cd7d5b57B7e401D7f1cA2E',
+  [ChainId.GÖRLI]: '0xBa60b6e6fF25488308789E6e0A65D838be34194e',
+  [ChainId.MAISTESTSUBNET]: MAISTESTSUBNET_MIXED_ROUTE_QUOTER_V1
+}
+
+export const UNISWAP_MULTICALL_ADDRESSES: AddressMap = {
+  ...constructSameAddressMap('0x1F98415757620B543A52E61c46B32eB19261F984'),
+  [ChainId.CELO]: CELO_MULTICALL_ADDRESS,
+  [ChainId.CELO_ALFAJORES]: CELO_MULTICALL_ADDRESS,
+  [ChainId.MAISTESTSUBNET]: MAISTESTSUBNET_MULTICALL
+
+  // TODO: Gnosis + Moonbeam contracts to be deployed
+};
+
+export const OVM_GASPRICE_ADDRESS =
+  '0x420000000000000000000000000000000000000F';
+export const ARB_GASINFO_ADDRESS = '0x000000000000000000000000000000000000006C';
+
+// export const TICK_LENS_ADDRESS = '0xbfd8137f7d1516D3ea5cA83523914859ec47F573';
+// export const NONFUNGIBLE_POSITION_MANAGER_ADDRESS =
+// '0xC36442b4a4522E871399CD717aBDD847Ab11FE88';
+// export const SWAP_ROUTER_ADDRESS = '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45';
+// export const V3_MIGRATOR_ADDRESS = '0xA5644E29708357803b5A882D272c41cC0dF92B34';
+// export const MULTICALL2_ADDRESS = '0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696';
+
+// maistestsubnet
+export const TICK_LENS_ADDRESS = '0x11Bce84Bb773Ee5F74f972f8D063a02Bc65226B0';
+export const NONFUNGIBLE_POSITION_MANAGER_ADDRESS =
+  '0xb2b7375CAf3961FA8aaB2E457713b3A077d9161d';
+export const SWAP_ROUTER_ADDRESS = '0x2038EBd53355fe56f46596a6D45d6807057705aa';
+export const V3_MIGRATOR_ADDRESS = '0xcC694091fc78cb3C139d7aeBB0420034E527B1C5';
+export const MULTICALL2_ADDRESS = '0xd24D14EDFDB29A0470096446414A09311ebad0E0';
+
+export type AddressMap = { [chainId: number]: string };
+
+export function constructSameAddressMap<T extends string>(
+  address: T,
+  additionalNetworks: ChainId[] = []
+): { [chainId: number]: T } {
+  return NETWORKS_WITH_SAME_UNISWAP_ADDRESSES.concat(
+    additionalNetworks
+  ).reduce<{
+    [chainId: number]: T;
+  }>((memo, chainId) => {
+    memo[chainId] = address;
+    return memo;
+  }, {});
+}
+
+export const WETH9: {
+  [chainId in Exclude<
+    ChainId,
+    | ChainId.POLYGON
+    | ChainId.POLYGON_MUMBAI
+    | ChainId.CELO
+    | ChainId.CELO_ALFAJORES
+    | ChainId.GNOSIS
+    | ChainId.MOONBEAM
+  >]: Token;
+} = {
+  [ChainId.MAINNET]: new Token(
+    ChainId.MAINNET,
+    '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    18,
+    'WETH',
+    'Wrapped Ether'
+  ),
+  [ChainId.ROPSTEN]: new Token(
+    ChainId.ROPSTEN,
+    '0xc778417E063141139Fce010982780140Aa0cD5Ab',
+    18,
+    'WETH',
+    'Wrapped Ether'
+  ),
+  [ChainId.RINKEBY]: new Token(
+    ChainId.RINKEBY,
+    '0xc778417E063141139Fce010982780140Aa0cD5Ab',
+    18,
+    'WETH',
+    'Wrapped Ether'
+  ),
+  [ChainId.GÖRLI]: new Token(
+    ChainId.GÖRLI,
+    '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
+    18,
+    'WETH',
+    'Wrapped Ether'
+  ),
+  [ChainId.KOVAN]: new Token(
+    ChainId.KOVAN,
+    '0xd0A1E359811322d97991E03f863a0C30C2cF029C',
+    18,
+    'WETH',
+    'Wrapped Ether'
+  ),
+  [ChainId.OPTIMISM]: new Token(
+    ChainId.OPTIMISM,
+    '0x4200000000000000000000000000000000000006',
+    18,
+    'WETH',
+    'Wrapped Ether'
+  ),
+  [ChainId.OPTIMISTIC_KOVAN]: new Token(
+    ChainId.OPTIMISTIC_KOVAN,
+    '0x4200000000000000000000000000000000000006',
+    18,
+    'WETH',
+    'Wrapped Ether'
+  ),
+  [ChainId.ARBITRUM_ONE]: new Token(
+    ChainId.ARBITRUM_ONE,
+    '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
+    18,
+    'WETH',
+    'Wrapped Ether'
+  ),
+  [ChainId.ARBITRUM_RINKEBY]: new Token(
+    ChainId.ARBITRUM_RINKEBY,
+    '0xB47e6A5f8b33b3F17603C83a0535A9dcD7E32681',
+    18,
+    'WETH',
+    'Wrapped Ether'
+  ),
+  [ChainId.MAISTESTSUBNET]: new Token(
+    ChainId.MAISTESTSUBNET,
+    '0x45F1836b76731AAc50E8e6fAbcA35872d9fbDF73',
+    18,
+    'WETH',
+    'Wrapped Ether'
+  ),
+};
